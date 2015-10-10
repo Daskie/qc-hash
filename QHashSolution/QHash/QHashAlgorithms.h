d78a387419bea67109749f982eb10d9c6f0d742e
Austin Quick <daskie.q@gmail.com>
2015-10-10 03:09:58 -0700

    Documented QHashAlgorithms.h

--- QHashSolution/QHash/QHashAlgorithms.h
+++ QHashSolution/QHash/QHashAlgorithms.h
@@ -1,3 +1,8 @@
+/*
+	Code written by Austin Quick.
+	Created August 2015.
+*/
+
 #pragma once
 
 #ifdef QHASH_EXPORTS
@@ -11,12 +16,18 @@
 
 namespace QHashAlgorithms {
 
+/*
+	Simply pairs key data and num key bytes.
+*/
 struct KeyBundle {
 	KeyBundle(const void * key, int nBytes) : key_(key), nBytes_(nBytes) {}
 	const void * key_;
 	int nBytes_;
 };
 
+/*
+	Base for custom key data interpretation.
+*/
 class KeyDecoder {
 	public:
 	virtual KeyBundle decode(KeyBundle kb) const {
@@ -26,6 +37,10 @@ class KeyDecoder {
 
 static const KeyDecoder DEFAULT_KEY_DECODER;
 
+/*
+	Interprets std::string data as string.c_str rather than object data.
+	Does not include the \0 to avoid repetition.
+*/
 class StringKeyDecoder : public KeyDecoder {
 	public:
 	virtual KeyBundle decode(KeyBundle kb) const {
@@ -38,26 +53,58 @@ class StringKeyDecoder : public KeyDecoder {
 
 static const StringKeyDecoder STRING_KEY_DECODER;
 
+/*
+	Simple pair to accommodate the 128 bit hash results.
+*/
 struct Hash128 { uint64_t h1; uint64_t h2; };
 
+/*
+	Interprets nKeyBytes worth of key data using murmur_x86_32 and returns the
+	hash.
+*/
 QHASH_API uint32_t hash32(const void * key, int nKeyBytes, uint32_t seed = 0);
 
+/*
+	Interprets the key string as c_str using murmur_x86_128 and returns the hash.
+*/
 QHASH_API uint32_t hash32(const std::string & key, uint32_t seed = 0);
 
+/*
+	Translator for KeyBundle support.
+*/
 QHASH_API uint32_t hash32(KeyBundle kb, uint32_t seed = 0);
 
+/*
+	Interprets nKeyBytes worth of key data using murmur_x86_32 and returns the
+	hash.
+*/
 QHASH_API Hash128 hash64(const void * key, int nKeyBytes, uint32_t seed = 0);
 
+/*
+	Interprets the key string as c_str using murmur_x86_128 and returns the hash.
+*/
 QHASH_API Hash128 hash64(const std::string & key, uint32_t seed = 0);
 
+/*
+	Translator for KeyBundle support.
+*/
 QHASH_API Hash128 hash64(KeyBundle kb, uint32_t seed = 0);
 
 namespace MurmurHash3 {
 
+/*
+	Hashes len bytes of key and writes a 32 bit result to out.
+*/
 QHASH_API void murmur_x86_32(const void * key, int len, uint32_t seed, void * out);
 
+/*
+	Hashes len bytes of key and writes two 64 bit results to out.
+*/
 QHASH_API void murmur_x86_128(const void * key, int len, uint32_t seed, void * out);
 
+/*
+	Hashes len bytes of key and writes two 64 bit results to out.
+*/
 QHASH_API void murmur_x64_128(const void * key, int len, uint32_t seed, void * out);
 
 }
