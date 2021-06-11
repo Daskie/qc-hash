ad8612c35e22aafb02ec5fca59b1fe1285347511
Austin Quick <daskie.q@gmail.com>
2021-06-11 03:59:47 -0600

    performance now prints out a csv format for graphing

--- performance/set/performance-set.cpp
+++ performance/set/performance-set.cpp
@@ -8,9 +8,11 @@
 #include <qc-core/random.hpp>
 #include <qc-core/vector.hpp>
 
+#include <qc-hash/qc-map-chunk.hpp>
 #include <qc-hash/qc-map-flat.hpp>
 #include <qc-hash/qc-map-orig.hpp>
 #include <qc-hash/qc-map-alt.hpp>
+#include <qc-hash/qc-map.hpp>
 
 using namespace qc::types;
 
@@ -49,42 +51,55 @@ static void printFactor(const s64 t1, const s64 t2) {
     std::cout << std::setw(6) << percent << " %";
 }
 
-struct Times {
-    s64 construct{};
-    s64 insert{};
-    s64 insertPresent{};
-    s64 accessPresent{};
-    s64 accessAbsent{};
-    s64 accessEmpty{};
-    s64 iterateFull{};
-    s64 iterateHalf{};
-    s64 iterateEmpty{};
-    s64 erase{};
-    s64 eraseAbsent{};
-    s64 refill{};
-    s64 clear{};
-    s64 loneBegin{};
-    s64 loneEnd{};
-
-    Times & operator+=(const Times & other) {
-        construct += other.construct;
-        insert += other.insert;
-        insertPresent += other.insertPresent;
-        accessPresent += other.accessPresent;
-        accessAbsent += other.accessAbsent;
-        accessEmpty += other.accessEmpty;
-        iterateFull += other.iterateFull;
-        iterateHalf += other.iterateHalf;
-        iterateEmpty += other.iterateEmpty;
-        erase += other.erase;
-        eraseAbsent += other.eraseAbsent;
-        refill += other.refill;
-        clear += other.clear;
-        loneBegin += other.loneBegin;
-        loneEnd += other.loneEnd;
-        return *this;
-    }
+enum class Operation : size_t {
+    construct,
+    insert,
+    insertPresent,
+    accessPresent,
+    accessAbsent,
+    accessEmpty,
+    iterateFull,
+    iterateHalf,
+    iterateEmpty,
+    erase,
+    eraseAbsent,
+    refill,
+    clear,
+    loneBegin,
+    loneEnd,
+    _n
 };
+
+static const std::array<std::string, size_t(Operation::_n)> operationNames{
+    "Construct",
+    "Insert",
+    "InsertPresent",
+    "AccessPresent",
+    "AccessAbsent",
+    "AccessEmpty",
+    "IterateFull",
+    "IterateHalf",
+    "iterateEmpty",
+    "Erase",
+    "EraseAbsent",
+    "Refill",
+    "Clear",
+    "LoneBegin",
+    "LoneEnd"
+};
+
+static Operation & operator++(Operation & op) {
+    return op = Operation(size_t(op) + 1u);
+}
+
+using Times = std::array<s64, size_t(Operation::_n)>;
+
+static Times & operator+=(Times & times1, const Times & times2) {
+    for (size_t i{}; i < times1.size(); ++i) {
+        times1[i] += times2[i];
+    }
+    return times1;
+}
 
 template <typename S, typename K>
 static Times time(const std::vector<K> & presentKeys, const std::vector<K> & nonpresentKeys) {
@@ -211,78 +226,86 @@ static Times time(const std::vector<K> & presentKeys, const std::vector<K> & non
 
     const s64 t18{now()};
 
-    return Times{
-        .construct = t1 - t0,
-        .insert = t2 - t1,
-        .insertPresent = t3 - t2,
-        .accessPresent = t4 - t3,
-        .accessAbsent = t5 - t4,
-        .accessEmpty = t11 - t10,
-        .iterateFull = t6 - t5,
-        .iterateHalf = t9 - t8,
-        .iterateEmpty = t12 - t11,
-        .erase = (t8 - t7) + (t10 - t9),
-        .eraseAbsent = t7 - t6,
-        .refill = t17 - t16,
-        .clear = t18 - t17,
-        .loneBegin = t14 - t13,
-        .loneEnd = t15 - t14
+    return {
+        t1 - t0,   // construct
+        t2 - t1,   // insert
+        t3 - t2,   // insertPresent
+        t4 - t3,   // accessPresent
+        t5 - t4,   // accessAbsent
+        t11 - t10, // accessEmpty
+        t6 - t5,   // iterateFull
+        t9 - t8,   // iterateHalf
+        t12 - t11, // iterateEmpty
+        (t8 - t7) + (t10 - t9), // erase
+        t7 - t6,   // eraseAbsent
+        t17 - t16, // refill
+        t18 - t17, // clear
+        t14 - t13, // loneBegin
+        t15 - t14  // loneEnd
     };
 }
 
-template <typename K, typename S1, typename S2>
-static std::pair<Times, Times> compareUnsaturated(const size_t elementCount, const size_t roundCount) {
+static void reportComparison(const Times & times1, const Times & times2) {
+    std::cout << "   Operation   | S1 Time | S2 Time | % Faster " << std::endl;
+    std::cout << "---------------+---------+---------+----------" << std::endl;
+    for (Operation op{}; op < Operation::_n; ++op) {
+        const s64 t1{times1[size_t(op)]};
+        const s64 t2{times2[size_t(op)]};
+
+        std::cout << std::setw(14) << operationNames[size_t(op)];
+        std::cout << " | ";
+        printTime(t1);
+        std::cout << " | ";
+        printTime(t2);
+        std::cout << " | ";
+        printFactor(t1, t2);
+        std::cout << std::endl;
+    }
+}
+
+static void printChartable(const std::vector<std::pair<std::string, Times>> & stats) {
+    std::cout << ','; for (const auto & [name, times] : stats) std::cout << name << ','; std::cout << std::endl;
+    for (Operation op{}; op < Operation::_n; ++op) {
+        std::cout << operationNames[size_t(op)] << ','; for (const auto & [name, times] : stats) std::cout << times[size_t(op)] << ','; std::cout << std::endl;
+    }
+}
+
+int main() {
+    const size_t elementCount{1000u};
+    const size_t roundCount{1000u};
+    using K = size_t;
+    //using H = qc_hash::config::DefaultHash<K>;
+    using S1 = std::unordered_set<K>;
+    using S2 = qc_hash_orig::Set<K>;
+    using S3 = qc_hash_chunk::Set<K>;
+    using S4 = qc_hash_flat::Set<K>;
+    using S5 = qc_hash::Set<K>;
+
     qc::Random random{};
     std::vector<K> presentKeys(elementCount);
     std::vector<K> nonpresentKeys(elementCount);
     for (K & key : presentKeys) key = random.next<K>();
-    Times result1{};
-    Times result2{};
+
+    std::vector<std::pair<std::string, Times>> setTimes{
+        {"std::unordered_set", {}},
+        {"qc::hash::OrigSet", {}},
+        {"qc::hash::ChunkSet", {}},
+        {"qc::hash::FlatSet", {}},
+        {"qc::hash::Set", {}}
+    };
 
     for (size_t round{0u}; round < roundCount; ++round) {
         std::swap(presentKeys, nonpresentKeys);
         for (K & key : presentKeys) key = random.next<K>();
 
-        result1 += time<S1>(presentKeys, nonpresentKeys);
-        result2 += time<S2>(presentKeys, nonpresentKeys);
+        setTimes[0].second += time<S1>(presentKeys, nonpresentKeys);
+        setTimes[1].second += time<S2>(presentKeys, nonpresentKeys);
+        setTimes[2].second += time<S3>(presentKeys, nonpresentKeys);
+        setTimes[3].second += time<S4>(presentKeys, nonpresentKeys);
+        setTimes[4].second += time<S5>(presentKeys, nonpresentKeys);
     }
 
-    return {result1, result2};
-}
-
-static void report(const std::pair<Times, Times> & results) {
-    const Times & res1{results.first};
-    const Times & res2{results.second};
-
-    std::cout << "   Operation   | S1 Time | S2 Time | % Faster " << std::endl;
-    std::cout << "---------------+---------+---------+----------" << std::endl;
-    std::cout << "   Construct   | "; printTime(res1.construct    ); std::cout << " | "; printTime(res2.construct    ); std::cout << " | "; printFactor(res1.construct    , res2.construct    ); std::cout << std::endl;
-    std::cout << "    Insert     | "; printTime(res1.insert       ); std::cout << " | "; printTime(res2.insert       ); std::cout << " | "; printFactor(res1.insert       , res2.insert       ); std::cout << std::endl;
-    std::cout << " InsertPresent | "; printTime(res1.insertPresent); std::cout << " | "; printTime(res2.insertPresent); std::cout << " | "; printFactor(res1.insertPresent, res2.insertPresent); std::cout << std::endl;
-    std::cout << " AccessPresent | "; printTime(res1.accessPresent); std::cout << " | "; printTime(res2.accessPresent); std::cout << " | "; printFactor(res1.accessPresent, res2.accessPresent); std::cout << std::endl;
-    std::cout << " AccessAbsent  | "; printTime(res1.accessAbsent ); std::cout << " | "; printTime(res2.accessAbsent ); std::cout << " | "; printFactor(res1.accessAbsent , res2.accessAbsent ); std::cout << std::endl;
-    std::cout << "  AccessEmpty  | "; printTime(res1.accessEmpty  ); std::cout << " | "; printTime(res2.accessEmpty  ); std::cout << " | "; printFactor(res1.accessEmpty  , res2.accessEmpty  ); std::cout << std::endl;
-    std::cout << "  IterateFull  | "; printTime(res1.iterateFull  ); std::cout << " | "; printTime(res2.iterateFull  ); std::cout << " | "; printFactor(res1.iterateFull  , res2.iterateFull  ); std::cout << std::endl;
-    std::cout << "  IterateHalf  | "; printTime(res1.iterateHalf  ); std::cout << " | "; printTime(res2.iterateHalf  ); std::cout << " | "; printFactor(res1.iterateHalf  , res2.iterateHalf  ); std::cout << std::endl;
-    std::cout << " IterateEmpty  | "; printTime(res1.iterateEmpty ); std::cout << " | "; printTime(res2.iterateEmpty ); std::cout << " | "; printFactor(res1.iterateEmpty , res2.iterateEmpty ); std::cout << std::endl;
-    std::cout << "     Erase     | "; printTime(res1.erase        ); std::cout << " | "; printTime(res2.erase        ); std::cout << " | "; printFactor(res1.erase        , res2.erase        ); std::cout << std::endl;
-    std::cout << "  EraseAbsent  | "; printTime(res1.eraseAbsent  ); std::cout << " | "; printTime(res2.eraseAbsent  ); std::cout << " | "; printFactor(res1.eraseAbsent  , res2.eraseAbsent  ); std::cout << std::endl;
-    std::cout << "    Refill     | "; printTime(res1.refill       ); std::cout << " | "; printTime(res2.refill       ); std::cout << " | "; printFactor(res1.refill       , res2.refill       ); std::cout << std::endl;
-    std::cout << "     Clear     | "; printTime(res1.clear        ); std::cout << " | "; printTime(res2.clear        ); std::cout << " | "; printFactor(res1.clear        , res2.clear        ); std::cout << std::endl;
-    std::cout << "   LoneBegin   | "; printTime(res1.loneBegin    ); std::cout << " | "; printTime(res2.loneBegin    ); std::cout << " | "; printFactor(res1.loneBegin    , res2.loneBegin    ); std::cout << std::endl;
-    std::cout << "    LoneEnd    | "; printTime(res1.loneEnd      ); std::cout << " | "; printTime(res2.loneEnd      ); std::cout << " | "; printFactor(res1.loneEnd      , res2.loneEnd      ); std::cout << std::endl;
-}
-
-int main() {
-    const bool saturateCache{false};
-    const size_t elementCount{10000u};
-    const size_t roundCount{1000u};
-    using K = size_t;
-    using H = qc_hash::config::DefaultHash<K>;
-    using S1 = std::unordered_set<K, H>;
-    using S2 = qc_hash::Set<K, H>;
-
-    report(compareUnsaturated<K, S1, S2>(elementCount, roundCount));
+    printChartable(setTimes);
 
     return 0;
 }
