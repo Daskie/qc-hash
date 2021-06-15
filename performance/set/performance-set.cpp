fc8d32764bd6eb1ee3aa65fca6aa4198ee2d0a85
Austin Quick <daskie.q@gmail.com>
2021-06-15 14:34:15 -0600

    added object and iterator size fields to performance stats

--- performance/set/performance-set.cpp
+++ performance/set/performance-set.cpp
@@ -21,38 +21,40 @@ static s64 now() {
     return std::chrono::nanoseconds(std::chrono::steady_clock::now().time_since_epoch()).count();
 }
 
-static void printTime(const s64 nanoseconds) {
+static void printTime(const s64 nanoseconds, const size_t width) {
     if (nanoseconds < 10000) {
-        std::cout << std::setw(4) << nanoseconds << " ns";
+        std::cout << std::setw(width - 3) << nanoseconds << " ns";
         return;
     }
 
     const s64 microseconds{(nanoseconds + 500) / 1000};
     if (microseconds < 10000) {
-        std::cout << std::setw(4) << microseconds << " us";
+        std::cout << std::setw(width - 3) << microseconds << " us";
         return;
     }
 
     const s64 milliseconds{(microseconds + 500) / 1000};
     if (milliseconds < 10000) {
-        std::cout << std::setw(4) << milliseconds << " ms";
+        std::cout << std::setw(width - 3) << milliseconds << " ms";
         return;
     }
 
     const s64 seconds{(milliseconds + 500) / 1000};
-    std::cout << std::setw(4) << seconds << " s ";
+    std::cout << std::setw(width - 3) << seconds << " s ";
 }
 
-static void printFactor(const s64 t1, const s64 t2) {
+static void printFactor(const s64 t1, const s64 t2, const size_t width) {
     const double absFactor{t1 >= t2 ? double(t1) / double(t2) : double(t2) / double(t1)};
     int percent{int(qc::round(absFactor * 100.0)) - 100};
     if (t1 < t2) {
         percent = -percent;
     }
-    std::cout << std::setw(6) << percent << " %";
+    std::cout << std::setw(width - 2) << percent << " %";
 }
 
-enum class Operation : size_t {
+enum class Stat : size_t {
+    objectSize,
+    iteratorSize,
     construct,
     insert,
     insertPresent,
@@ -71,7 +73,9 @@ enum class Operation : size_t {
     _n
 };
 
-static const std::array<std::string, size_t(Operation::_n)> operationNames{
+static const std::array<std::string, size_t(Stat::_n)> statNames{
+    "ObjectSize",
+    "IteratorSize",
     "Construct",
     "Insert",
     "InsertPresent",
@@ -86,24 +90,24 @@ static const std::array<std::string, size_t(Operation::_n)> operationNames{
     "Refill",
     "Clear",
     "LoneBegin",
-    "LoneEnd"
+    "LoneEnd",
 };
 
-static Operation & operator++(Operation & op) {
-    return op = Operation(size_t(op) + 1u);
+static Stat & operator++(Stat & op) {
+    return op = Stat(size_t(op) + 1u);
 }
 
-using Times = std::array<s64, size_t(Operation::_n)>;
+using Stats = std::array<s64, size_t(Stat::_n)>;
 
-static Times & operator+=(Times & times1, const Times & times2) {
-    for (size_t i{}; i < times1.size(); ++i) {
-        times1[i] += times2[i];
+static Stats & operator+=(Stats & stats1, const Stats & stats2) {
+    for (size_t i{}; i < stats1.size(); ++i) {
+        stats1[i] += stats2[i];
     }
-    return times1;
+    return stats1;
 }
 
 template <typename S, typename K>
-static Times time(const std::vector<K> & presentKeys, const std::vector<K> & nonpresentKeys) {
+static Stats time(const std::vector<K> & presentKeys, const std::vector<K> & nonpresentKeys) {
     static volatile size_t v{};
 
     const std::span<const K> firstHalfPresentKeys{&presentKeys[0], presentKeys.size() / 2};
@@ -228,6 +232,8 @@ static Times time(const std::vector<K> & presentKeys, const std::vector<K> & non
     const s64 t18{now()};
 
     return {
+        0,         // objectSize
+        0,         // iteratorSize
         t1 - t0,   // construct
         t2 - t1,   // insert
         t3 - t2,   // insertPresent
@@ -246,78 +252,81 @@ static Times time(const std::vector<K> & presentKeys, const std::vector<K> & non
     };
 }
 
-static void reportComparison(const std::pair<std::string, Times> & setTimes1, const std::pair<std::string, Times> & setTimes2) {
-    static const std::string c1Header{"Operation"};
+static void reportComparison(const std::pair<std::string, Stats> & setStats1, const std::pair<std::string, Stats> & setStats2) {
+    static const std::string c1Header{"Stat"};
     static const std::string c4Header{"% Faster"};
 
     size_t c1Width{c1Header.size()};
-    for (Operation op{}; op < Operation::_n; ++op) {
-        qc::maxify(c1Width, operationNames[size_t(op)].size());
+    for (Stat op{}; op < Stat::_n; ++op) {
+        qc::maxify(c1Width, statNames[size_t(op)].size());
     }
-    const size_t c2Width{qc::max(setTimes1.first.size(), size_t(7u))};
-    const size_t c3Width{qc::max(setTimes2.first.size(), size_t(7u))};
+    const size_t c2Width{qc::max(setStats1.first.size(), size_t(7u))};
+    const size_t c3Width{qc::max(setStats2.first.size(), size_t(7u))};
     const size_t c4Width{qc::max(c4Header.size(), size_t(8u))};
 
-    std::cout << std::format(" {:^{}} | {:^{}} | {:^{}} | {:^{}} ", "Operation", c1Width, setTimes1.first, c2Width, setTimes2.first, c3Width, "% Faster", c4Width) << std::endl;
+    std::cout << std::format(" {:^{}} | {:^{}} | {:^{}} | {:^{}} ", c1Header, c1Width, setStats1.first, c2Width, setStats2.first, c3Width, c4Header, c4Width) << std::endl;
     std::cout << "---------------+---------+---------+----------" << std::endl;
-    for (Operation op{}; op < Operation::_n; ++op) {
-        const s64 t1{setTimes1.second[size_t(op)]};
-        const s64 t2{setTimes2.second[size_t(op)]};
+    for (Stat op{}; op < Stat::_n; ++op) {
+        const s64 t1{setStats1.second[size_t(op)]};
+        const s64 t2{setStats2.second[size_t(op)]};
 
-        std::cout << std::format(" {:^{}} | ", operationNames[size_t(op)], c1Width);
-        printTime(t1);
+        std::cout << std::format(" {:^{}} | ", statNames[size_t(op)], c1Width);
+        printTime(t1, c2Width);
         std::cout << " | ";
-        printTime(t2);
+        printTime(t2, c3Width);
         std::cout << " | ";
-        printFactor(t1, t2);
+        printFactor(t1, t2, c4Width);
         std::cout << std::endl;
     }
 }
 
-static void printChartable(const std::vector<std::pair<std::string, Times>> & stats) {
-    std::cout << ','; for (const auto & [name, times] : stats) std::cout << name << ','; std::cout << std::endl;
-    for (Operation op{}; op < Operation::_n; ++op) {
-        std::cout << operationNames[size_t(op)] << ','; for (const auto & [name, times] : stats) std::cout << times[size_t(op)] << ','; std::cout << std::endl;
+static void printChartable(const std::vector<std::pair<std::string, Stats>> & setStats) {
+    std::cout << ','; for (const auto & [name, stats] : setStats) std::cout << name << ','; std::cout << std::endl;
+    for (Stat stat{}; stat < Stat::_n; ++stat) {
+        std::cout << statNames[size_t(stat)] << ','; for (const auto & [name, stats] : setStats) std::cout << stats[size_t(stat)] << ','; std::cout << std::endl;
     }
 }
 
 int main() {
     const size_t elementCount{1000u};
-    const size_t roundCount{1000u};
+    const size_t roundCount{5000u};
     using K = size_t;
     //using H = qc_hash::config::DefaultHash<K>;
     using S1 = std::unordered_set<K>;
     using S2 = qc_hash_orig::Set<K>;
     using S3 = qc_hash_chunk::Set<K>;
     using S4 = qc_hash_flat::Set<K>;
-    using S5 = qc_hash::Set<K>;
+    using S5 = qc_hash_alt::Set<K>;
+    using S6 = qc_hash::Set<K>;
 
     qc::Random random{};
     std::vector<K> presentKeys(elementCount);
     std::vector<K> nonpresentKeys(elementCount);
     for (K & key : presentKeys) key = random.next<K>();
 
-    std::vector<std::pair<std::string, Times>> setTimes{
-        {"std::unordered_set", {}},
-        {"qc::hash::OrigSet", {}},
-        {"qc::hash::ChunkSet", {}},
-        {"qc::hash::FlatSet", {}},
-        {"qc::hash::Set", {}}
+    std::vector<std::pair<std::string, Stats>> setStats{
+        {"std::unordered_set", {sizeof(S1), sizeof(S1::iterator)}},
+        {"qc::hash::OrigSet",  {sizeof(S2), sizeof(S2::iterator)}},
+        {"qc::hash::ChunkSet", {sizeof(S3), sizeof(S3::iterator)}},
+        {"qc::hash::FlatSet",  {sizeof(S4), sizeof(S4::iterator)}},
+        {"qc::hash::AltSet",   {sizeof(S5), sizeof(S5::iterator)}},
+        {"qc::hash::Set",      {sizeof(S6), sizeof(S6::iterator)}}
     };
 
     for (size_t round{0u}; round < roundCount; ++round) {
         std::swap(presentKeys, nonpresentKeys);
         for (K & key : presentKeys) key = random.next<K>();
 
-        setTimes[0].second += time<S1>(presentKeys, nonpresentKeys);
-        setTimes[1].second += time<S2>(presentKeys, nonpresentKeys);
-        setTimes[2].second += time<S3>(presentKeys, nonpresentKeys);
-        setTimes[3].second += time<S4>(presentKeys, nonpresentKeys);
-        setTimes[4].second += time<S5>(presentKeys, nonpresentKeys);
+        setStats[0].second += time<S1>(presentKeys, nonpresentKeys);
+        setStats[1].second += time<S2>(presentKeys, nonpresentKeys);
+        setStats[2].second += time<S3>(presentKeys, nonpresentKeys);
+        setStats[3].second += time<S4>(presentKeys, nonpresentKeys);
+        setStats[4].second += time<S5>(presentKeys, nonpresentKeys);
+        setStats[5].second += time<S6>(presentKeys, nonpresentKeys);
     }
 
-    //reportComparison(setTimes[3], setTimes[4]);
-    printChartable(setTimes);
+    //reportComparison(setStats[4], setStats[5]);
+    printChartable(setStats);
 
     return 0;
 }
